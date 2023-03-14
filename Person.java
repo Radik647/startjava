40489db93c97e7f5bb6e5c82448b82f020014441
Radik647 <120384783+Radik647@users.noreply.github.com>
2023-03-14 14:42:32 +0300

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,24 @@
+public class Person {
+    
+    String Name = "Jake";
+    String floor = "male";
+    float height = 5,6f;
+    float weight = 0,3f;
+    float years old = 26;
+
+    void move() {
+        System.out.prinln("moving");
+    }
+
+    void sit() {
+        System.out.prinln("sitting");
+    }
+
+    void run() {
+        System.out.prinln("run around");
+    }
+
+    void speak() {
+        System.out.prinln("speaking");
+    }
+}
